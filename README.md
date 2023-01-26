353bb2f8af14f51036de524507e3f8f2b55d3e4f
killer snake <123300427+PallavJain01@users.noreply.github.com>
2023-01-26 23:40:54 +0530

    solved more questions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #### Repository created to store the practice questions i have done while learning python
 #### questions are from [zhiwehu's repository](https://github.com/zhiwehu/Python-programming-exercises) which contains all these questions
+
+##### [Solutions](https://github.com/PallavJain01/Python-practice-questions/blob/main/solutions.py)
+##### [Main file](https://github.com/PallavJain01/Python-practice-questions/blob/main/main.py)
--- solutions.py
+++ solutions.py
@@ -6,6 +6,8 @@ import re
 # solution: ...
 
 # Q.1- level 1
+# Question: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
 # Question: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200
 # (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 
@@ -18,8 +20,8 @@ def ans_1():
 
 
 # Q.2- level 1
-# Question: Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated
-# sequence on a single line. Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
+# Question: Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
 
 def ans_2():
   num = int(input())
@@ -33,9 +35,8 @@ def ans_2():
 
 
 # Q.3- level 1
-# Question: With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an
-# integral number between 1 and n (both included). and then the program should print the dictionary. Suppose the following
-# input is supplied to the program: 8 Then, the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+# Question: With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included)
+# and then the program should print the dictionary. Suppose the following input is supplied to the program: 8 Then, the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 
 def ans_3():
   num = input()
@@ -58,8 +59,8 @@ def ans_4():
 
 
 # Q.5- level 1
-# Question: Define a class which has at least two methods: getString: to get a string from console input printString: to print the
-# string in upper case. Also please include simple test function to test the class methods.
+# Question: Define a class which has at least two methods: getString: to get a string from console input printString: to print the string in upper case.
+# Also please include simple test function to test the class methods.
 
 class ans_5(object):
   def __init__(self):
@@ -73,10 +74,8 @@ class ans_5(object):
 
 
 # Q.6- level 2
-# Question: Write a program that calculates and prints the value according to the given formula: Q = Square root of [(2 * C * D)/H]
-# Following are the fixed values of C and H: C is 50. H is 30. D is the variable whose values should be input to your program in a
-# comma-separated sequence. Example Let us assume the following comma separated input sequence is given to the program: 100,150,180
-# The output of the program should be: 18,22,24
+# Question: Write a program that calculates and prints the value according to the given formula: Q = Square root of [(2 * C * D)/H].
+#Following are the fixed values of C and H: C is 50. H is 30. D is the variable whose values should be input to your program in a comma-separated sequence. Example Let us assume the following comma separated input sequence is given to the program: 100,150,180 The output of the program should be: 18,22,24
 
 def ans_6():
   nums = input()
@@ -91,9 +90,8 @@ def ans_6():
 
 
 # Q.7- level 2
-# Question: Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row
-# and j-th column of the array should be i*j. Note: i=0,1.., X-1; j=0,1,¡­Y-1. Example Suppose the following inputs are given to the program:
-# 3,5 Then, the output of the program should be: 
+# Question: Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+# Note: i=0,1.., X-1; j=0,1,Y-1. Example Suppose the following inputs are given to the program: 3,5 Then, the output of the program should be:
 # [
 #  [0, 0, 0, 0, 0],
 #  [0, 1, 2, 3, 4],
@@ -115,9 +113,8 @@ def ans_7():
 
 
 # Q.8- level 2
-# Question: Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence
-# after sorting them alphabetically. Suppose the following input is supplied to the program: without,hello,bag,world Then, the output should be:
-# bag,hello,without,world
+# Question: Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Suppose the following input is supplied to the program: without,hello,bag,world Then, the output should be: bag,hello,without,world
 
 def ans_8():
   inp = input()
@@ -128,8 +125,7 @@ def ans_8():
 
 # Q.9- level 2
 # Question: Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
-# Suppose the following input is supplied to the program: Hello world Practice makes perfect Then, the output should be:
-# HELLO WORLD PRACTICE MAKES PERFECT
+# Suppose the following input is supplied to the program: Hello world Practice makes perfect Then, the output should be: HELLO WORLD PRACTICE MAKES PERFECT
 
 def ans_9():
   text_f = ""
@@ -140,9 +136,8 @@ def ans_9():
 
 
 # Q.10- level 2
-# Question: Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all
-# duplicate words and sorting them alphanumerically. Suppose the following input is supplied to the program: hello world and practice
-# makes perfect and hello world again Then, the output should be: again and hello makes perfect practice world
+# Question: Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and
+# sorting them alphanumerically. Suppose the following input is supplied to the program: hello world and practice makes perfect and hello world again Then, the output should be: again and hello makes perfect practice world
 
 def ans_10():
   words = [i for i in input().split(" ")]
